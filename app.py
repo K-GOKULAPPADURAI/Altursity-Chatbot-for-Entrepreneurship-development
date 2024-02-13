3f1722ccbdb87e03f51eda5dddfd6780b60faceb
APPAtacker.py <77343301+K-GOKULAPPADURAI@users.noreply.github.com>
2024-02-14 00:10:18 +0530

    Update and rename run.py to app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from time import sleep
 os.system('cmd /c "pip install -r requirements.txt"')
 
 
-directory = __file__[:-len('run.py')]
+directory = __file__[:-len('app.py')]
 os.chdir(directory)
 os.system('cmd /c "python train.py"')
 
